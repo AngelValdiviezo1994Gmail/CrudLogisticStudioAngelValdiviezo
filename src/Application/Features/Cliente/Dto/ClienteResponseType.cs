25169f111f175e34ca830aecbb04a6279727087e
AngelValdiviezo1994Gmail <131912926+AngelValdiviezo1994Gmail@users.noreply.github.com>
2024-05-18 11:23:43 -0500

    Primera carga del test

---
+++ src/Application/Features/Cliente/Dto/ClienteResponseType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AngelValdiviezoWebApi.Application.Features.Cliente.Dto
+{
+    public class ClienteResponseType
+    {
+        [JsonPropertyName("mensajeRespuesta")]
+        public string MensajeRespuesta { get; set; }
+    }
+}
---
+++ src/Persistence/Repository/CustomRepositoryAsync.cs
@@ -0,0 +1,21 @@
+﻿using AngelValdiviezoWebApi.Application.Common.Interfaces;
+using AngelValdiviezoWebApi.Persistence.Contexts;
+using Ardalis.Specification.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngelValdiviezoWebApi.Persistence.Repository
+{
+    public class CustomRepositoryAsync<T> : RepositoryBase<T>, IRepositoryAsync<T> where T : class
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CustomRepositoryAsync(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+    }
+}
