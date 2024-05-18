6735f3b04711926048ef1be8861ff8f5498450be
AngelValdiviezo1994Gmail <131912926+AngelValdiviezo1994Gmail@users.noreply.github.com>
2024-05-18 16:08:27 -0500

    Consumo de apis

--- Module.Security.Infraestructure/Client/AutenticacionClient.cs
+++ Module.Security.Infraestructure/Client/AutenticacionClient.cs
@@ -15,10 +15,26 @@ namespace Module.Security.Infraestructure.Client
 
         public async Task<Response<UserDTO>> Loggin(Request<LoginRequestDTO> request)
         {
+            /*
             var response = await _httpClient.GetAsync("https://api.example.com/data");
             response.EnsureSuccessStatusCode();
+            */
+            using (HttpClient client = new HttpClient())
+            {
+                /*
+                 
+                  var content = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = await client.PostAsync("https://localhost:5001/api/example", content);
+       
+                 */
+                client.BaseAddress = new Uri("https://localhost:7203/");
+                HttpResponseMessage response = await client.GetAsync("api/v1/Token/CreateToken");
+                response.EnsureSuccessStatusCode();
 
-            //await response.Content.ReadAsStringAsync();
+                string responseData = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseData);
+            }
+
             return new Response<UserDTO>();
         }
     }
